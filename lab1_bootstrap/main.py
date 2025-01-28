2542d852f378a350ffba2a327de08e91a1b671c8
techyberme <107142731+techyberme@users.noreply.github.com>
2025-01-28 17:44:29 +0100

    initial commit

---
+++ lab1_bootstrap/main.py
@@ -0,0 +1,11 @@
+from src.dtw_lab.lab1 import read_csv_from_google_drive, visualize_data, calculate_statistic
+
+
+
+if __name__=='__main__':
+    df = read_csv_from_google_drive('1eKiAZKbWTnrcGs3bqdhINo1E4rBBpglo')
+    visualize_data(df)
+
+    print(f'The mean value for the charge left percentage is {calculate_statistic('mean',df['Charge_Left_Percentage'])}')
+
+    
