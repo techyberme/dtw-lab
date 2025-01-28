39ae737c3e27d5ccefeb22a371a811f78daaeaee
techyberme <107142731+techyberme@users.noreply.github.com>
2025-01-28 18:40:17 +0100

    prueba fallo

--- lab1_bootstrap/main.py
+++ lab1_bootstrap/main.py
@@ -6,6 +6,6 @@ if __name__=='__main__':
     df = read_csv_from_google_drive('1eKiAZKbWTnrcGs3bqdhINo1E4rBBpglo')
     visualize_data(df)
 
-    print(f'The mean value for the charge left percentage is {calculate_statistic('mean',df['Charge_Left_Percentage'])}')
+    print(f'The mean value for the charge left % is {calculate_statistic('mean',df['Charge_Left_Percentage'])}')
 
     
