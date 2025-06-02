c06fbabaf054668c6d7429e5fcb46b6623f5ba8d
techyberme <107142731+techyberme@users.noreply.github.com>
2025-06-02 13:45:29 +0200

    hola

--- src/dtw_lab/lab2.py
+++ src/dtw_lab/lab2.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pathlib import Path
 import toml
+from fastapi.responses import JSONResponse
 from dtw_lab.lab1 import (
     read_csv_from_google_drive,
     visualize_data,
@@ -20,3 +21,27 @@ def run_server(port: int = 80, reload: bool = False, host: str = "127.0.0.1"):
 @app.get("/")
 def main_route():
     return {"message": "Hello world"}
+@app.get("/statistic/{measure}/{column}")
+def get_statistic(measure: str, column: str):
+    df = read_csv_from_google_drive('1eKiAZKbWTnrcGs3bqdhINo1E4rBBpglo')
+    df = clean_data(df)
+    calculate_statistic('measure',df[column])
+@app.get("/visualize/{graph_type}")
+def get_visualization(graph_type: str):
+    df = read_csv_from_google_drive('1eKiAZKbWTnrcGs3bqdhINo1E4rBBpglo')
+    df = clean_data(df)
+    visualize_data(df)
+    return FileResponse("graphs/{graph_type}.png", filename="{graph_type}.png")
+@app.get("/version")
+def get_visualization_version():
+    pyproject_path = Path(__file__).parent.parent / "pyproject.toml"
+    if not pyproject_path.exists():
+        return JSONResponse(status_code=404, content={"error": "pyproject.toml not found"})
+
+    pyproject_data = toml.load(pyproject_path)
+    version = pyproject_data.get("tool", {}).get("poetry", {}).get("version")
+
+    if version is None:
+        return JSONResponse(status_code=500, content={"error": "Version not found in pyproject.toml"})
+
+    return {"version": version}
